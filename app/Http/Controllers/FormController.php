c4ae08a82788e4004519bbb65cf3a97fc8a60cf7
Sergei Sanzharov <66815199+SanzharovSergei@users.noreply.github.com>
2023-02-23 08:09:42 +0300

    new
    new file

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function index()
+    {
+        $forms = Form::latest()->get();
+        return view('forms.index', ['forms' => $forms]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('forms.create');
+    }
+
+    public function store()
+    {
+        $this->validateRequestForm();
+        $form = new Form();
+
+        $form->username = request('username');
+        $form->comment = request('comment');
+
+        $form->save();
+
+        return redirect('/forms'); // заменить на список комментариев
+    }
+
+    public function show(Form $form)
+    {
+        return view('forms.show', ['form' => $form]);
+    }
+
+    protected function validateRequestForm()
+    {
+        return request()->validate([
+            'username' => 'required|min:3',
+            'comment' => 'required'
+        ]);
+    }
+
+}
+
+
